680186d2885b0f9e3e730020936289133b44c70c
Matt Ford <matt.ford@activepipe.com>
2016-12-06 11:18:18 +1100

    Updates

--- emailer/send.php
+++ emailer/send.php
@@ -5,26 +5,51 @@
 
         $name   = "Matt";
         // $email  = "matt.t.ford@gmail.com";
-        
-        $email = "elizabeth.cooke@abbvie.com";
+        // $email = "astrid.macassey@abbvie.com";
 
         $mail = new PHPMailer(true);
         
         $mail->IsSMTP();
         $mail->IsHTML(true);
-
-        $mail->AddAddress($email);
-                
-        $body = file_get_contents('index.html');
-
-        $mail->SMTPAuth = true;
-        $mail->Port     = 587;
-        $mail->Host     = 'smtp.mandrillapp.com';
-        $mail->Username = 'shlomi@h-t.com.au';
-        $mail->Password = '0phzsVf8hOEKpGcVkhWuBw';
-        $mail->From     = 'au_donotreply@h-t.com.au';
-        $mail->FromName = 'Test sender';
-        $mail->Subject  = "Email test";
-        $mail->Body     = $body;
         
-        $mail->send();
+try {
+    // $mail->AddAddress($email);
+
+    // $mail->AddAddress('poonam.chapke@abbvie.com');
+    $mail->AddAddress('mattf@h-t.com.au');
+    $mail->AddAddress('matt.t.ford@gmail.com');
+    // $mail->AddAddress('lordsnot@gmail.com');
+    // $mail->AddAddress('danielle.corones@abbvie.com');
+    // $mail->AddAddress('vanessa.cowley@abbvie.com');
+    // $mail->AddAddress('ally.crystal@abbvie.com');
+    // $mail->AddAddress('mike.durban@abbvie.com');
+    // $mail->AddAddress('linette.haak@abbvie.com');
+    // $mail->AddAddress('amanda.kiersey@abbvie.com');
+    // $mail->AddAddress('lucy.lay@abbvie.com');
+    // $mail->AddAddress('vicki.mccaugherty@abbvie.com');
+    // $mail->AddAddress('marnie.myles@abbvie.com');
+    // $mail->AddAddress('claire.sanders@abbvie.com');
+    // $mail->AddAddress('joanne.taylor@abbvie.com');
+    // $mail->AddAddress('nikki.vaughan@abbvie.com');
+    // $mail->AddAddress('poonam.chapke@abbvie.com');
+            
+    $body = file_get_contents('index.html');
+
+    $mail->SMTPAuth = true;
+    $mail->Port     = 587;
+    $mail->Host     = 'smtp.mandrillapp.com';
+    $mail->Username = 'shlomi@h-t.com.au';
+    $mail->Password = '0phzsVf8hOEKpGcVkhWuBw';
+    $mail->From     = 'au_donotreply@h-t.com.au';
+    $mail->FromName = 'H&T EDM Preview';
+    $mail->Subject  = "HUMIRA Ulcerative Colitis PBS announcement";
+    $mail->Body     = $body;
+    // $mail->SMTPDebug = 1;
+    var_dump($mail->send());
+    echo "Message Sent OK\n";
+} catch (phpmailerException $e) {
+  echo $e->errorMessage(); //Pretty error messages from PHPMailer
+} catch (Exception $e) {
+  echo $e->getMessage(); //Boring error messages from anything else!
+}
+  
