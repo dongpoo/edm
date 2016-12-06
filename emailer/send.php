629dd999da846da9fb32b8887bf4b13644e5d66a
Matt <mattf@h-t.com.au>
2016-12-06 15:47:49 +1100

    Tolmar letter done

--- emailer/send.php
+++ emailer/send.php
@@ -13,26 +13,9 @@
         $mail->IsHTML(true);
         
 try {
-    // $mail->AddAddress($email);
 
-    // $mail->AddAddress('poonam.chapke@abbvie.com');
-    $mail->AddAddress('mattf@h-t.com.au');
     $mail->AddAddress('matt.t.ford@gmail.com');
-    // $mail->AddAddress('lordsnot@gmail.com');
-    // $mail->AddAddress('danielle.corones@abbvie.com');
-    // $mail->AddAddress('vanessa.cowley@abbvie.com');
-    // $mail->AddAddress('ally.crystal@abbvie.com');
-    // $mail->AddAddress('mike.durban@abbvie.com');
-    // $mail->AddAddress('linette.haak@abbvie.com');
-    // $mail->AddAddress('amanda.kiersey@abbvie.com');
-    // $mail->AddAddress('lucy.lay@abbvie.com');
-    // $mail->AddAddress('vicki.mccaugherty@abbvie.com');
-    // $mail->AddAddress('marnie.myles@abbvie.com');
-    // $mail->AddAddress('claire.sanders@abbvie.com');
-    // $mail->AddAddress('joanne.taylor@abbvie.com');
-    // $mail->AddAddress('nikki.vaughan@abbvie.com');
-    // $mail->AddAddress('poonam.chapke@abbvie.com');
-            
+   
     $body = file_get_contents('index.html');
 
     $mail->SMTPAuth = true;
@@ -53,3 +36,24 @@ try {
   echo $e->getMessage(); //Boring error messages from anything else!
 }
   
+
+
+    // $mail->AddAddress($email);
+
+    // $mail->AddAddress('poonam.chapke@abbvie.com');
+    // $mail->AddAddress('mattf@h-t.com.au');
+
+      // $mail->AddAddress('lordsnot@gmail.com');
+    // $mail->AddAddress('danielle.corones@abbvie.com');
+    // $mail->AddAddress('vanessa.cowley@abbvie.com');
+    // $mail->AddAddress('ally.crystal@abbvie.com');
+    // $mail->AddAddress('mike.durban@abbvie.com');
+    // $mail->AddAddress('linette.haak@abbvie.com');
+    // $mail->AddAddress('amanda.kiersey@abbvie.com');
+    // $mail->AddAddress('lucy.lay@abbvie.com');
+    // $mail->AddAddress('vicki.mccaugherty@abbvie.com');
+    // $mail->AddAddress('marnie.myles@abbvie.com');
+    // $mail->AddAddress('claire.sanders@abbvie.com');
+    // $mail->AddAddress('joanne.taylor@abbvie.com');
+    // $mail->AddAddress('nikki.vaughan@abbvie.com');
+    // $mail->AddAddress('poonam.chapke@abbvie.com');
